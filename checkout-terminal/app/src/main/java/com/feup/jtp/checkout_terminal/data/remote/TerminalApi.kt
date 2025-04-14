236a7239623bc3973d1b312ca0e0710b0bd9a15b
Henrique Gardé <up202108725@up.pt>
2025-04-14 14:51:25 +0100

    TerminalAPI

--- checkout-terminal/app/src/main/java/com/feup/jtp/checkout_terminal/data/remote/TerminalApi.kt
+++ checkout-terminal/app/src/main/java/com/feup/jtp/checkout_terminal/data/remote/TerminalApi.kt
@@ -1,20 +1,14 @@
 package com.feup.jtp.checkout_terminal.data.remote
 
 import com.feup.jtp.checkout_terminal.data.model.dto.TransactionToServer
+import com.feup.jtp.checkout_terminal.data.model.dto.TransactionFromServer
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface TerminalApi {
 
     @POST("/api/checkout")
     suspend fun checkoutCart(
         @Body request: TransactionToServer
-    ): TransactionToServer
-
-    @GET("/api/cart")
-    suspend fun getCartForUser(
-        @Query("uuid") uuid: String,
-    ): CartDto
+    ): TransactionFromServer
 }

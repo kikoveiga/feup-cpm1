f912305bbdd46bf25094744231008f256f21d95d
José Francisco Veiga <veigajosefrancisco@gmail.com>
2025-04-30 23:36:10 +0100

    Update agp

--- client-app/app/src/main/java/com/feup/client/di/NetworkModule.kt
+++ client-app/app/src/main/java/com/feup/client/di/NetworkModule.kt
@@ -32,8 +32,7 @@ object NetworkModule {
     fun provideRetrofit(client: OkHttpClient, gson: Gson): Retrofit {
         val baseUrl =
             if (isEmulator()) "http://10.0.2.2:8080/"
-            else "http://192.168.1.185:8080/" // Replace with your IP address
-
+            else "http://192.168.1.242:8080/" // Replace with your IP address
 
         return Retrofit.Builder()
             .baseUrl(baseUrl)

195b9c34f49441e9f69849a958cccd28a881c5cb
José Francisco Veiga <veigajosefrancisco@gmail.com>
2025-05-02 21:34:57 +0100

    Server improvements

--- electronic-supermarket-server/src/main/kotlin/com/acme/supermarket/server/controller/PastRecordsController.kt
+++ electronic-supermarket-server/src/main/kotlin/com/acme/supermarket/server/controller/PastRecordsController.kt
@@ -1,11 +1,14 @@
 package com.acme.supermarket.server.controller
 
-import com.acme.supermarket.server.dto.*
+import com.acme.supermarket.server.dto.PastRecordsRequestDto
+import com.acme.supermarket.server.dto.TransactionDto
 import com.acme.supermarket.server.service.PastRecordsService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
-import kotlin.math.sign
 
 @RestController
 @RequestMapping("/api")
--- electronic-supermarket-server/src/main/kotlin/com/acme/supermarket/server/service/PastRecordsService.kt
+++ electronic-supermarket-server/src/main/kotlin/com/acme/supermarket/server/service/PastRecordsService.kt
@@ -9,6 +9,7 @@ import com.acme.supermarket.server.repository.UserRepository
 import com.acme.supermarket.server.repository.VoucherRepository
 import org.apache.coyote.BadRequestException
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 import java.security.SecureRandom
 import java.util.*
 
@@ -79,5 +80,24 @@ class PastRecordsService(
         return vouchers.map { it.uuid }
     }
 
+    fun verifyAndCalculateAccumulatedDiscount(request: PastRecordsRequestDto): BigDecimal {
+
+        val user = userRepository.findByUserUuid(request.userUuid)
+            ?: throw BadRequestException("User not found")
+
+        val messageToVerify = "userUuid:${request.userUuid}&nonce:${request.nonce}"
+
+        val isValid = cryptoService.verifyEcSignature(
+            publicKeyBase64 = user.ecPublicKey,
+            message = messageToVerify,
+            signatureBase64 = request.signature
+        )
+
+        if (!isValid) {
+            throw BadRequestException("Invalid signature.")
+        }
+
+        return user.accumulatedDiscount
+    }
 }
 

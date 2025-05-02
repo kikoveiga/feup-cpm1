67b3ef44d0a75d23a7456424c12b70c1d573c1ed
Joao Padrao <up202108766@up.pt>
2025-05-02 18:26:22 +0100

    Fixed generate qr code

--- client-app/app/src/main/java/com/feup/client/domain/usecases/GenerateTransactionQrUseCase.kt
+++ client-app/app/src/main/java/com/feup/client/domain/usecases/GenerateTransactionQrUseCase.kt
@@ -2,7 +2,6 @@ package com.feup.client.domain.usecases
 
 import com.feup.client.data.dto.TransactionToServerDto
 import com.feup.client.domain.crypto.CryptoManager
-import com.feup.client.domain.local.UserDataStore
 import com.feup.client.domain.model.Product
 import com.feup.client.domain.model.Transaction
 import com.feup.client.domain.model.Voucher
@@ -12,20 +11,15 @@ import javax.inject.Inject
 
 class GenerateTransactionQrUseCase @Inject constructor(
     private val cryptoManager: CryptoManager,
-    private val userDataStore: UserDataStore
 ) {
-    suspend fun invoke(
+    fun invoke(
         userUuid: String,
         products: List<Product>,
         useAccumulatedDiscount: Boolean,
         voucherId: String?
     ): TransactionToServerDto {
         val date = System.currentTimeMillis().toString()
-        val nickname = userDataStore.getLoggedInUser().nickname
-        val signature = cryptoManager.generateSignature(
-            userNickname = nickname,
-            "asdasd".toByteArray()
-        )
+       
         return TransactionToServerDto(
             userUuid = userUuid,
             date = date,
